4b46bdb06257bed38c85f07e4c1b459654ca5a42
Mike Penz <mikepenz@gmail.com>
2020-10-13 08:59:52 +0200

    - fix message undefined issue - add test cases to ensure this is not reintroduced

--- utils.js
+++ utils.js
@@ -61,8 +61,8 @@ async function parseFile(file) {
                 ).trim();
 
                 const message = (
-                    (testcase.failure && testcase.failure._attributes.message) ||
-                    (testcase.error && testcase.error._attributes.message) ||
+                    (testcase.failure && testcase.failure._attributes && testcase.failure._attributes.message) ||
+                    (testcase.error && testcase.error._attributes && testcase.error._attributes.message) ||
                     stackTrace.split('\n').slice(0, 2).join('\n')
                 ).trim();
 
